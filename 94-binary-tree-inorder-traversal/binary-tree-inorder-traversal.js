92785e5991c596c92c6c777b117902d460ec75cb
Mukesh Tiwary <84659547+Mukesh-17@users.noreply.github.com>
2024-12-02 01:47:00 +0530

    Time: 0 ms (100.00%) | Memory: 49.5 MB (10.19%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -24,3 +24,27 @@ var inorderTraversal = function(root) {
     inTraverse(root);
     return ans;
 };
+
+//Iterative approach
+var inorderTraversal = function(root) {
+    const result = [];
+    const stack = [];
+    let current = root;
+
+    while (current || stack.length > 0) {
+        // Reach the leftmost node of the current node
+        while (current) {
+            stack.push(current);
+            current = current.left;
+        }
+        
+        // Current must be null at this point
+        current = stack.pop();
+        result.push(current.val);  // Visit root
+        
+        // Now visit the right subtree
+        current = current.right;
+    }
+    
+    return result;
+};
