9647f51686149adfad41d8b1b1935ab2bef764be
Mukesh Tiwary <84659547+Mukesh-17@users.noreply.github.com>
2024-12-08 02:56:34 +0530

    Time: 0 ms (100.00%) | Memory: 52.1 MB (46.75%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(!head || !head.next)
+        return head;
+        let prev=null,curr=head;
+
+        while(curr!==null){
+            let nextTemp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=nextTemp;
+
+        }
+        return prev;
+};
