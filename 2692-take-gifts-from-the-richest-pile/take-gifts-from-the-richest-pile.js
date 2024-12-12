b8a07e66ae25c56c097cca2e43020d068914b9dc
Mukesh Tiwary <84659547+Mukesh-17@users.noreply.github.com>
2024-12-12 23:56:05 +0530

    Time: 106 ms (32.20%) | Memory: 55.2 MB (22.89%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} gifts
+ * @param {number} k
+ * @return {number}
+ */
+const pickGifts = (gifts, k) => {
+
+
+    for(let i = 0; i < k; i++){
+        gifts.sort((a,b) => a - b)
+        let newEl = Math.floor(Math.sqrt(gifts.pop()))
+        gifts.push(newEl)
+    }
+    return gifts.reduce((a,b) => a + b)
+};
